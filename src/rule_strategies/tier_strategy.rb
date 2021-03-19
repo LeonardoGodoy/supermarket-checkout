5f080b82b936dd67d50865c23fb61e3a4b3df50b
Leonardo Godoy <leonardo.godoy.souza@gmail.com>
2021-03-19 20:00:43 -0300

    Rename variables to make it clear

--- src/rule_strategies/tier_strategy.rb
+++ src/rule_strategies/tier_strategy.rb
@@ -1,24 +1,24 @@
 module RuleStrategies
   class TierStrategy
-    attr_reader :config
+    attr_reader :price_tiers
 
-    def initialize(config)
-      @config = config
+    def initialize(price_tiers)
+      @price_tiers = price_tiers
     end
 
     def calculate(quantity, _source=nil)
       amount = 0
       remaining = quantity
 
-      config.keys.sort.reverse_each do |tier|
-        cost = config[tier]
+      price_tiers.keys.sort.reverse_each do |tier|
+        cost = price_tiers[tier]
         next if remaining.zero?
 
-        groups = remaining / tier
-        next if groups.zero?
+        tier_repetitions = remaining / tier
+        next if tier_repetitions.zero?
 
         remaining = remaining % tier
-        amount += groups * cost
+        amount += tier_repetitions * cost
       end
 
       amount
