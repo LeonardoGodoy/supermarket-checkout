c66e57acd501e8a32f61143169a7388396cbcde0
Leonardo Godoy <leonardo.godoy.souza@gmail.com>
2021-03-19 19:51:06 -0300

    Rename methods

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -9,36 +9,36 @@ RSpec.describe CheckOut do
   ]
 
   def price(goods)
-    co = CheckOut.new(RULES)
-    goods.split(//).each { |item| co.scan(item) }
-    co.total
+    checkout = CheckOut.new(RULES)
+    goods.split(//).each { |item| checkout.scan(item) }
+    checkout.total
   end
 
-  def expects_items_sum(items, total)
+  def expects_total_items_sum(items, total)
     expect(price(items)).to be_eql(total)
   end
 
-  def expects_total_adding(checkout, item, total)
+  def expects_total_adding_item(checkout, item, total)
     checkout.scan(item)
     expect(checkout.total).to be_eql(total)
   end
 
   describe '#total' do
     context 'with all items in a row' do
-      it { expects_items_sum('A', 50) }
-      it { expects_items_sum('AB', 80) }
-      it { expects_items_sum('CDBA', 115) }
+      it { expects_total_items_sum('A', 50) }
+      it { expects_total_items_sum('AB', 80) }
+      it { expects_total_items_sum('CDBA', 115) }
 
-      it { expects_items_sum('AA', 100) }
-      it { expects_items_sum('AAA', 130) }
-      it { expects_items_sum('AAAA', 180) }
-      it { expects_items_sum('AAAAA', 230) }
-      it { expects_items_sum('AAAAAA', 260) }
+      it { expects_total_items_sum('AA', 100) }
+      it { expects_total_items_sum('AAA', 130) }
+      it { expects_total_items_sum('AAAA', 180) }
+      it { expects_total_items_sum('AAAAA', 230) }
+      it { expects_total_items_sum('AAAAAA', 260) }
 
-      it { expects_items_sum('AAAB', 160) }
-      it { expects_items_sum('AAABB', 175) }
-      it { expects_items_sum('AAABBD', 190) }
-      it { expects_items_sum('DABABA', 190) }
+      it { expects_total_items_sum('AAAB', 160) }
+      it { expects_total_items_sum('AAABB', 175) }
+      it { expects_total_items_sum('AAABBD', 190) }
+      it { expects_total_items_sum('DABABA', 190) }
     end
 
     context 'when scanning items one by one' do
@@ -46,11 +46,11 @@ RSpec.describe CheckOut do
         checkout = CheckOut.new(RULES)
 
         expect(checkout.total).to be_eql(0)
-        expects_total_adding(checkout, 'A', 50)
-        expects_total_adding(checkout, 'B', 80)
-        expects_total_adding(checkout, 'A', 130)
-        expects_total_adding(checkout, 'A', 160)
-        expects_total_adding(checkout, 'B', 175)
+        expects_total_adding_item(checkout, 'A', 50)
+        expects_total_adding_item(checkout, 'B', 80)
+        expects_total_adding_item(checkout, 'A', 130)
+        expects_total_adding_item(checkout, 'A', 160)
+        expects_total_adding_item(checkout, 'B', 175)
       end
     end
   end
